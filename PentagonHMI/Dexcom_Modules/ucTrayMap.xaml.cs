d14aa4bdf78b77b95edb2b69764483a736b8a9c3
Teoh Hock Chuan <teoh.hock.chuan@pentamaster.com.my>
2022-05-13 17:36:32 +0800

    Changing made in TrayMap page.

--- PentagonHMI/Dexcom_Modules/ucTrayMap.xaml.cs
+++ PentagonHMI/Dexcom_Modules/ucTrayMap.xaml.cs
@@ -21,14 +21,21 @@ namespace PentagonHMI.ChildControls
         const string Tag_R_Reinspect = "ISL0_RightRack_HMIReq_InspectTray";
         const string Tag_L_ChangeTray = "ISL0_HMI_LeftRack_ReqChangeTray";
         const string Tag_R_ChangeTray = "ISL0_HMI_RightRack_ReqChangeTray";
+        const string Tag_PCBA_ChangeTray = "HMI_PCBA_ReqChangeTray";
+        const string Tag_Battery_ChangeTray = "HMI_Battery_ReqChangeTray";
         const string Tag_L_PurgeRack = "ISL0_HMI_LeftRack_PurgeReq";
         const string Tag_R_PurgeRack = "ISL0_HMI_RightRack_PurgeReq";
         const string Tag_TrayRow = "HMI_Tags.TrayRowNo";
         const string Tag_TrayCol = "HMI_Tags.TrayColumnNo";
         const string Tag_L_TopVision = "ISL0_TopVision_LeftTrayMap[0]";//Length 100;
         const string Tag_R_TopVision = "ISL0_TopVision_LeftTrayMap[0]";//Length 100;
+        const string Tag_Btry_dint = "Lot_Info.HMI_BatteryType";
         int Row = 0;
         int Col = 0;
+        int PRow = 0;
+        int PCol = 0;
+        int BRow = 0;
+        int BCol = 0;
 
         Dictionary<string, Brush> Dic_ResultColor = new Dictionary<string, Brush>();
 
@@ -48,9 +55,41 @@ namespace PentagonHMI.ChildControls
 #if !DEBUG
             Row = OPCore.Read<int>(Tag_TrayRow);
             Col = OPCore.Read<int>(Tag_TrayCol);
+            PRow = OPCore.Read<int>(Tag_TrayRow);
+            PCol = OPCore.Read<int>(Tag_TrayCol);
+            BRow = OPCore.Read<int>(Tag_TrayRow);
+            BCol = OPCore.Read<int>(Tag_TrayCol);
+            var Battery_Type = OPCore.Read<int>(Tag_Btry_dint);
 #else
-            Row = 10;
-            Col = 10;
+            Row = 4;
+            Col = 4;
+            PRow = 9;
+            PCol = 10;
+
+            var Battery_Type = 5;  
+
+            switch(Battery_Type)
+            {
+                //Maxell
+                case 2:
+                    BRow = 10;
+                    BCol = 10;
+                    break;
+                //Panasonic
+                case 5:
+                    BRow = 4;
+                    BCol = 10;
+                    break;
+                //Murata
+                case 6:
+                    BRow = 9;
+                    BCol = 10;
+                    break;
+                default:
+                    MessageBox.Show("Failed to read battery type from PLC.");
+                    break;
+            }
+
 #endif
 
             BrushConverter bc = new BrushConverter();
@@ -70,10 +109,10 @@ namespace PentagonHMI.ChildControls
             ugrd_LeftTray.Columns = Col;
             ugrd_RightTray.Rows = Row;
             ugrd_RightTray.Columns = Col;
-            pcba_lot_track.Rows = Row;
-            pcba_lot_track.Columns = Col;
-            bat_slot_track.Rows = Row;
-            bat_slot_track.Columns = Col;
+            pcba_SlotTray.Rows = PRow;
+            pcba_SlotTray.Columns = PCol;
+            bat_SlotTray.Rows = BRow;
+            bat_SlotTray.Columns = BCol;
 
 
             int total = Row * Col;
@@ -81,8 +120,18 @@ namespace PentagonHMI.ChildControls
             {
                 ugrd_LeftTray.Children.Add(new TextBlock { Tag = t, Text = "-" });
                 ugrd_RightTray.Children.Add(new TextBlock { Tag = t, Text = "-" });
-                pcba_SlotTray.Children.Add(new TextBlock { Tag = t, Text = "-" });
-                bat_SlotTray.Children.Add(new TextBlock { Tag = t, Text = "-" });
+            }
+
+            int totalPCBA = PRow * PCol;
+            for (int tp = 1; tp <= totalPCBA; tp++)
+            {
+                pcba_SlotTray.Children.Add(new TextBlock { Tag = tp, Text = "-" });
+            }
+
+            int totalBattery = BRow * BCol;
+            for (int tb = 1; tb <= totalBattery; tb++)
+            {
+                bat_SlotTray.Children.Add(new TextBlock { Tag = tb, Text = "-" });
             }
         }
 
@@ -97,6 +146,7 @@ namespace PentagonHMI.ChildControls
                     var RAry = OPCore.Read<int[]>(Tag_R_TopVision, typeof(int), 100);
                     var PCBAray = OPCore.Read<int[]>(Tag_L_TopVision, typeof(int), 100);
                     var BatAray = OPCore.Read<int[]>(Tag_R_TopVision, typeof(int), 100);
+
 
                     if (LAry != null)
                         foreach (var item in ugrd_LeftTray.Children)
@@ -115,18 +165,18 @@ namespace PentagonHMI.ChildControls
                             tb.Background = Dic_ResultColor[result];
                         }
                     if (PCBAray != null)
-                        foreach (var item in ugrd_LeftTray.Children)
+                        foreach (var item in pcba_SlotTray.Children)
                         {
                             TextBlock tb = item as TextBlock;
-                            string result = LAry[Convert.ToInt32(tb.Tag) - 1].ToString();
+                            string result = PCBAray[Convert.ToInt32(tb.Tag) - 1].ToString();
                             tb.Text = result;
                             tb.Background = Dic_ResultColor[result];
                         }
                     if (BatAray != null)
-                        foreach (var item in ugrd_RightTray.Children)
+                        foreach (var item in bat_SlotTray.Children)
                         {
                             TextBlock tb = item as TextBlock;
-                            string result = RAry[Convert.ToInt32(tb.Tag) - 1].ToString();
+                            string result = BatAray[Convert.ToInt32(tb.Tag) - 1].ToString();
                             tb.Text = result;
                             tb.Background = Dic_ResultColor[result];
                         }
@@ -157,7 +207,7 @@ namespace PentagonHMI.ChildControls
         private void ChangeTray_Click(object sender, RoutedEventArgs e)
         {
             string tag = (sender as Button).Tag.ToString();
-            OPCore.Write(tag == "L" ? Tag_L_ChangeTray : tag == "R" ? Tag_R_ChangeTray : "", true);
+            OPCore.Write(tag == "L" ? Tag_L_ChangeTray : tag == "R" ? Tag_R_ChangeTray : tag == "pcba" ? Tag_R_ChangeTray : tag == "battery" ? Tag_R_ChangeTray : "", true);
         }
 
         private void Purge_Click(object sender, DependencyPropertyChangedEventArgs e)
