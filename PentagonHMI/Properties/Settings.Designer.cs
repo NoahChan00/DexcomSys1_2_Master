7512fa2af60a4f62686f3bb726e1c6e42deffb27
Leon <lau.chun.chee@pentamaster.com.my>
2022-08-04 15:56:03 +0800

    Change lengend font to white

--- PentagonHMI/Properties/Settings.Designer.cs
+++ PentagonHMI/Properties/Settings.Designer.cs
@@ -326,8 +326,8 @@ namespace PentagonHMI.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("persist security info=false; data source=UN-FP-CL-P2451\\SQLEXPRESS; integrated se" +
-            "curity=true; initial catalog=gdb_dexcomsystem2;")]
+        [global::System.Configuration.DefaultSettingValueAttribute("data source=UN-FP-CL-P2451\\SQLEXPRESS; integrated security=true; initial catalog=" +
+            "gdb_dexcomsystem1;")]
         public string DatabaseConnectionString {
             get {
                 return ((string)(this["DatabaseConnectionString"]));
