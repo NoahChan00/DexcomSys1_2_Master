97e74a6d553eaacabdd32bb8841e5a6db024b00f
Leon <lau.chun.chee@pentamaster.com.my>
2022-08-17 14:10:43 +0800

    Remove battery lot for pick fail and adjust oee layout

--- PentagonHMI/Main.xaml.cs
+++ PentagonHMI/Main.xaml.cs
@@ -231,7 +231,7 @@ namespace PentagonHMI
         private void CheckPickFail()
         {
             bool allFalse = true;
-            foreach(var s in GlobalFunctions.IsSystem1 ? new List<string> { Tags.MainPage.PickFailPromptWindowPCBA.Name, Tags.MainPage.PickFailPromptWindowBattery.Name, } : new List<string> { Tags.MainPage.PickFailPromptWindowLShuttle.Name, Tags.MainPage.PickFailPromptWindowRShuttle.Name })
+            foreach(var s in GlobalFunctions.IsSystem1 ? new List<string> { Tags.MainPage.PickFailPromptWindowPCBA.Name, /*Tags.MainPage.PickFailPromptWindowBattery.Name,*/ } : new List<string> { Tags.MainPage.PickFailPromptWindowLShuttle.Name, Tags.MainPage.PickFailPromptWindowRShuttle.Name })
             {
                 bool readedState = _Main.OPC.Read<bool>(s);
                 if(readedState)
@@ -273,7 +273,6 @@ namespace PentagonHMI
                 pickFailPrompt = null;
                 MainGrid.IsEnabled = true;
                 PickFailCanva.Visibility = Visibility.Collapsed;
-                //PickFailContentControl.Content = pickFailPrompt;
             }
         }
 
